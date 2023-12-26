f1053b62e01b7d50d9dc6bede1eb81fe60d613e2
janghs <gorae930301@gmail.com>
2023-12-26 15:10:51 +0900

    lunchTimeScreenLock

---
+++ lunchBreak/run.bat
@@ -0,0 +1,4 @@
+@echo off
+@REM 경로의 해당 프로젝트가 위치한 루트를 작성하면 된다.
+@REM FullPath를 작성하는 이유는 어디서나 사용할 수 있도록 만들기 위함이다.
+start javaw -jar C:\lunchTimeScreenLock\lunchBreak\target\lunchBreak-0.0.1-SNAPSHOT.jar
